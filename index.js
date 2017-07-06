277104d6f8e651814cc54773a2b50b969cd4cb27
Jason Martin <jason@digital-pioneers.com>
2017-07-06 11:16:10 -0400

    fix login.

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
+import cookie from "cookie";
 
 const app = express();
 
-const cookie = 'ahncookiecheck=Cookie; ASPSESSIONIDCCAADADQ=BBKBEKDDPBDFGIGNBDLFEFPO; ASPSESSIONIDCCAADADQ=BBKBEKDDPBDFGIGNBDLFEFPO; _ga=GA1.2.1709541301.1498849320; _gid=GA1.2.410086980.1498849320'
+///THIS MAY HAVE TO BE REFRESHED PERIODICALLY
+var cookies = "pw=TAAYVTQC2B; uid=7165SHEFFIELD; ASPSESSIONIDCSTTBSQC=LHODJDCDHMBOJIABPMKOLCIP; ASPSESSIONIDASTTDSRC=LLOLFFCDIMMFOCEBKOPICPHF; _gat=1; _ga=GA1.2.1869469472.1476660900; _gid=GA1.2.324590848.1499350984; ahncookiecheck=Cookie; ASPSESSIONIDCSTTBSQC=LHODJDCDHMBOJIABPMKOLCIP; ASPSESSIONIDASTTDSRC=LLOLFFCDIMMFOCEBKOPICPHF";
 
+
+//Get Login page
 axios.get('http://polohoa.net/main.asp', {
-  headers: {'Header': cookie,
-            'Referer' : 'http://polohoa.net/login.asp?a=1&ahn=1234&bType=&id='}
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+	headers: { 'Cookie': cookies }})
+	.then(function (response)
+	{
+		console.log("***************** MAIN PAGE *****************");
+		console.log(response.data);
+		
+
+	})
+	.catch(function (error) { console.log(error); });
+
+
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
